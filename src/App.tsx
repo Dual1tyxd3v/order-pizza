7603bad5f2227694702d37b7d792fa96e89599f1
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-24 18:58:44 +0700

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './UI/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/order/CreateOrder';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/order/:id',
+    element: <Order />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+]);
 
 function App() {
-  return <p>hello world</p>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
